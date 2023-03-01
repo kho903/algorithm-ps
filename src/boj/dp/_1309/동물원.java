e21fc870c7c0b0873889f77fe3963b45f4cc4510
Kim Ji Hun <gmldnr2222@naver.com>
2023-03-01 20:56:23 +0900

    feat: boj / dp / 1309

---
+++ src/boj/dp/_1309/동물원.java
@@ -0,0 +1,24 @@
+package boj.dp._1309;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 동물원 {
+	static final int MOD = 9901;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		long[][] dp = new long[N + 1][3];
+		dp[1][0] = 1;
+		dp[1][1] = 1;
+		dp[1][2] = 1;
+		for (int i = 2; i <= N; i++) {
+			dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i -1][2]) % MOD;
+			dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+			dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) %MOD;
+		}
+		System.out.println((dp[N][0] + dp[N][1] + dp[N][2]) % MOD);
+	}
+}
