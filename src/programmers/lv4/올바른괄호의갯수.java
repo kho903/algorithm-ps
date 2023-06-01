c5a104f6a3a1ab2d7bf0ecc73e13cf6144f275db
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-01 20:07:14 +0900

    feat: programmers / lv4 / 올바른괄호의갯수

---
+++ src/programmers/lv4/올바른괄호의갯수.java
@@ -0,0 +1,18 @@
+package programmers.lv4;
+
+public class 올바른괄호의갯수 {
+	public int solution(int n) {
+
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				dp[i] += dp[i - j] * dp[j - 1];
+			}
+		}
+
+		return dp[n];
+	}
+}
