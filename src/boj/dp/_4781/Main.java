70b124682dffca70517d89d81c079582cf66d9d9
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-07 17:54:18 +0900

    feat: boj / dp / _4781

---
+++ src/boj/dp/_4781/Main.java
@@ -0,0 +1,23 @@
+package boj.dp._4781;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			int n = sc.nextInt();
+			int m = (int) Math.round(sc.nextDouble() * 100);
+			if (n == 0 && m == 0) break;
+			int[] dp = new int[m + 1];
+			for (int i = 0; i < n; i++) {
+				int c = sc.nextInt();
+				int p = (int) Math.round(sc.nextDouble() * 100);
+				for (int j = p; j <= m; j++) {
+					dp[j] = Math.max(dp[j], dp[j - p] + c);
+				}
+			}
+			System.out.println(dp[m]);
+		}
+	}
+}
