0bd413882baa70ba9d0a6cb54b040cd2496931d2
kho903 <gmldnr2222@naver.com>
2023-11-06 23:12:32 +0900

    feat: boj / dp / 9184

---
+++ src/boj/dp/_9184/Main.java
@@ -0,0 +1,47 @@
+package boj.dp._9184;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[][][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        dp = new int[21][21][21];
+        StringBuilder sb = new StringBuilder();
+        String co = ", ";
+        while (true) {
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+            int c = Integer.parseInt(stk.nextToken());
+            if (a == -1 && b == -1 && c == -1) break;
+            sb.append("w(").append(a).append(co).append(b).append(co).append(c)
+                .append(") = ").append(w(a,b,c)).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+
+
+    private static int w(int a, int b, int c) {
+        if (checked(a, b, c) && dp[a][b][c] != 0) return dp[a][b][c];
+        if (a <= 0 || b <= 0 || c <= 0) return 1;
+        if (a > 20 || b > 20 || c > 20) return dp[20][20][20] = w(20, 20, 20);
+        if (a < b && b < c)
+            return dp[a][b][c] =
+                w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+
+        return dp[a][b][c] =
+            w(a - 1, b, c)
+                + w(a - 1, b - 1, c)
+                + w(a - 1, b, c - 1)
+                - w(a - 1, b - 1, c - 1);
+    }
+
+    private static boolean checked(int a, int b, int c) {
+        return 0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20;
+    }
+}
